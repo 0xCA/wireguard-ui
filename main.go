5d4f4611b2cae8dc33500c97d4eefe65c34d9bac
0xCA <undefined>
2023-11-17 17:50:58 +0500

    SendRequestedConfigsToTelegram func, simplified dependencies, fixes

--- main.go
+++ main.go
@@ -216,10 +216,8 @@ func main() {
 	app.GET(util.BasePath+"/static/*", echo.WrapHandler(http.StripPrefix(util.BasePath+"/static/", assetHandler)))
 
 	initDeps := telegram.TgBotInitDependencies{
-		DB:                      db,
-		BuildClientConfig:       util.BuildClientConfig,
-		TgUseridToClientID:      util.TgUseridToClientID,
-		TgUseridToClientIDMutex: &util.TgUseridToClientIDMutex,
+		DB:                             db,
+		SendRequestedConfigsToTelegram: util.SendRequestedConfigsToTelegram,
 	}
 
 	initTelegram(initDeps)
